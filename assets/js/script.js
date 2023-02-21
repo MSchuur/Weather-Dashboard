35f3e504b0ce23ce14973b337fa91819c2935ae7
MSchuur <mschuur31@gmail.com>
2023-02-21 15:57:04 -0400

    Added the API to get the current and 5 day forcast for the city name entered.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,39 @@
 // Varibles requied for the app
 var searchCityEl = document.getElementById('user-form');
-var cityInputEl = document.getElementById('citySearch');
+var cityInputEl = document.getElementById('cityname');
 
 // Search for a city and get the lat and long
  function formCityHandler() {
    
-    console.log('submit requested')
     var cityname = cityInputEl.value.trim();
-
+    
     if (cityname) {
-        getLatAndLong(cityname);
+        getCurrentTemp(cityname);
+        getFiveDayForecast(cityname);
     }
     else {
         alert('The city name you entered is not valid. Please try again');
     }
 }
 
-getLatAndLong = function(city) {
-    var geoApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid={46d9562c80440e2c512be53d86040084}';
+function getCurrentTemp(city) {
+    var curApiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=46d9562c80440e2c512be53d86040084';
+    
+    fetch(curApiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+}
+
+function getFiveDayForecast(city) {
+    var geoApiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=46d9562c80440e2c512be53d86040084';
 
     fetch(geoApiUrl)
         .then(function (response) {
-            return response.jason();
+            return response.json();
         })
         .then(function (data) {
             console.log(data);
